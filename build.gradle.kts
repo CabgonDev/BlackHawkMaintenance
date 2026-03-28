53dc3f561985d42c4a59e261d16d5696c403d195
CabgonDev <ccabrera.e92@gmail.com>
2026-03-27 20:33:37 -0600

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.google.services) apply false
-
-}
